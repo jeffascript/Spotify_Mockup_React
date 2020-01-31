3e9333dd7820a2ddb6208a6470580d122e6844e8
jeffascript <ofoaro.jeff@gmail.com>
2020-01-31 16:19:20 +0100

    react app template

---
+++ D5 (2).js
@@ -0,0 +1,17 @@
+/*
+
+    Module 8 - D5 Team Work
+
+    Organize with your team to have a common version of React Spotify connected to the RapidAPI endpoint.
+    Once you have it, transform the application by using Redux stores in order to maintain the status of the application.
+
+    Features:
+    - Home page, Album page, artist page
+    - When clicking on a song, the name and the details should appear in the "player"
+    - Next to each song, you should be able to "Like" the song. From that moment on, wherever the song appears, the like "status" should be mantained
+    
+    Hints:
+    - Use the store for saving the song the user liked and check accordingly everytime you display a song
+    - Use the store for keeping track of the song that is currently playing.
+
+*/
